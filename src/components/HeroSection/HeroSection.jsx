0be6b3c42020eefb6fe0d28ec79d78f4c84615e0
Parsanta <parsantarajpal123@gmail.com>
2024-03-08 22:27:53 +0500

    banner sorted

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,13 +1,16 @@
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import image from '../HeroSection/image1.jpg';
+import image1 from '../HeroSection/image1.jpg';
+import image2 from '../HeroSection/image2.jpg';
+import image3 from '../HeroSection/image3.jpg';
+
 function HeroSection() {
   return (
     <Carousel className='w-100 px-sm-5 px-md-5 py-0'>
-      <Carousel.Item interval={1000} className='h-80'>
+      <Carousel.Item interval={1000} className='h-96'>
         <img
           className="d-block w-100"
-          src={image}
+          src={image1}
           alt="First slide"
         />
         <Carousel.Caption>
@@ -15,10 +18,10 @@ function HeroSection() {
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500} className='h-80'>
+      <Carousel.Item interval={500} className='h-96'>
       <img
           className="d-block w-100"
-          src={image}
+          src={image2}
           alt="Second slide"
         />
         <Carousel.Caption>
@@ -26,10 +29,10 @@ function HeroSection() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item className='h-80'>
+      <Carousel.Item className='h-96'>
       <img
           className="d-block w-100"
-          src={image}
+          src={image3}
           alt="third slide"
         />
         <Carousel.Caption>
